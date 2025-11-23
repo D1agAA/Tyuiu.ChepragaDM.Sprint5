e0e6b1fe48cf3682420be07c7da0efaa9be3ff02
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-11-23 14:52:34 +0500

    Add project files.

---
+++ Tyuiu.ChepragaDM.Sprint5.Task0.V23.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.ChepragaDM.Sprint5.Task0.V23.Lib
+{
+    public class DataService : ISprint5Task0V23
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask0.txt";
+
+            double y = (1.0 + Math.Pow(x, 3)) / Math.Pow(x, 2);
+
+            y = Math.Round(y, 3);
+
+            File.WriteAllText(path, Convert.ToString(y));
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ChepragaDM.Sprint5.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Tyuiu.ChepragaDM.Sprint5.Task0.V23.Lib;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+namespace Tyuiu.ChepragaDM.Sprint5.Task0.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int x = 3;
+
+            string path1 = @"C:\Users";
+            string path2 = @"D1agAAA\source\repos\Tyuiu.ChepragaDM.Sprint5\Tyuiu.ChepragaDM.Sprint5.Task0.V23\bin\Debug\net8.0\OutPutFileTask0.txt";
+            string fpath = Path.Combine(path1, path2);
+
+            FileInfo file = new FileInfo(fpath);
+            bool fileE = file.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileE);
+
+        }
+    }
+}
